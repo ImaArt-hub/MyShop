eeb8484ec4c2d80c05ae2101a34c64bd2bbcfac6
ImaArt-hub <55407921+ImaArt-hub@users.noreply.github.com>
2020-11-21 16:30:35 -0600

    Product Model and InMemory Product Repository

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public String Id { get; set; }
+        [DisplayName("Product Name")]
+        [StringLength(20)]
+        public String Name { get; set; }
+        public String Dscription { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public String Catergary { get; set;}
+        public String Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
