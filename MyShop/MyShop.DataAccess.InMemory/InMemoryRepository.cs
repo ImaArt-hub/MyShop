e2f31370417932ab897d3a26f20e30f819f46323
ImaArt-hub <55407921+ImaArt-hub@users.noreply.github.com>
2020-12-07 09:00:58 -0600

    Impiment dependancy injection.

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T>where T: BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -38,7 +39,7 @@ namespace MyShop.DataAccess.InMemory
         public void Update(T t)
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
-            if (tToUpdate!=null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -50,8 +51,8 @@ namespace MyShop.DataAccess.InMemory
 
         public T Find(string Id)
         {
-            T t = items.Find(i => i.Id ==Id);
-            if (t!= null)
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
             {
                 return t;
             }
@@ -68,10 +69,10 @@ namespace MyShop.DataAccess.InMemory
 
         public void Delete(string Id)
         {
-            T tToDelete = items.Find(i => i.Id ==Id);
+            T tToDelete = items.Find(i => i.Id == Id);
             if (tToDelete != null)
             {
-               items.Remove( tToDelete) ;
+                items.Remove(tToDelete);
             }
             else
             {
