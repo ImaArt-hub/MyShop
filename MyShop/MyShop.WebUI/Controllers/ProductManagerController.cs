afd50c660913cf478ea7166e243a321429d37040
ImaArt-hub <55407921+ImaArt-hub@users.noreply.github.com>
2020-12-10 17:20:58 -0600

    Added Product Images upload

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,7 +42,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -49,6 +50,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 Context.Insert(product);
                 Context.Commit();
                 return RedirectToAction("Index");
@@ -76,7 +82,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, String Id)
+        public ActionResult Edit(Product product, String Id,HttpPostedFileBase file)
         {
             Product productToEdit = Context.Find(Id);
             if (productToEdit == null)
@@ -89,10 +95,15 @@ namespace MyShop.WebUI.Controllers
                 {
                     return View(product);
                 }
+                if (file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                }
                 productToEdit.Catergory = product.Catergory;
                 productToEdit.Dscription = product.Dscription;
                 productToEdit.Name = product.Name;
-                productToEdit.Image = product.Image;
+               
                 productToEdit.Price = product.Price;
 
                 Context.Commit();
