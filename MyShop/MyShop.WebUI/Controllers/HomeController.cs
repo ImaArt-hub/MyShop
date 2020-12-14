8ccfb2d728c4eba2c9a7811b63e76ebac914a9fa
ImaArt-hub <55407921+ImaArt-hub@users.noreply.github.com>
2020-12-14 17:57:13 -0600

    Created Product Filter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +19,26 @@ namespace MyShop.WebUI.Controllers
             productCategories = productCategoriesContext;
             Context = ProductContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
 
-            List<Product> products = Context.Collection().ToList();
+            List<Product> products ;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
 
-            return View(products); 
+            if (Category == null)
+            {
+                products = Context.Collection().ToList();
+            }
+            else
+            {
+                products = Context.Collection().Where(p => p.Catergory == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model); 
         }
         public ActionResult Details(string Id)
         {
