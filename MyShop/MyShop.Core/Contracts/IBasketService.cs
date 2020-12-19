3684f70556b79107a7b3e012d45f50b331d494d0
ImaArt-hub <55407921+ImaArt-hub@users.noreply.github.com>
2020-12-19 12:50:01 -0600

    Created BasketService.

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItem(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBackSummery(HttpContextBase httpContext);
+    }
+}
